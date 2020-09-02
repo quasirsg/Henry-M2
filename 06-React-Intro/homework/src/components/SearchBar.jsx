99e6aefa2238a5bfe81e96859f0fc0509bd51c07
Romano Javier <alexis_b13@live.com.ar>
2020-09-02 11:41:52 -0300

    06 completo

--- 06-React-Intro/homework/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React from "react";
 
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
-};
+  return (
+    <div>
+      <div class="form-inline md-form mr-auto mb-4">
+        <input
+          class="form-control mr-sm-2"
+          type="text"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button onClick={props.onSearch} class="btn aqua-gradient btn-rounded btn-sm my-0" >
+          Search
+        </button>
+      </div>
+    </div>
+  );
+}
