ea493bff98ae2de3bfa9b84d7e4c2d87efcdad84
Romano Javier <alexis_b13@live.com.ar>
2020-09-04 11:54:00 -0300

    07

--- 07-React-Estilos/homework/src/components/Card.jsx
+++ 07-React-Estilos/homework/src/components/Card.jsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import React from "react";
+import { Button } from "react-bootstrap";
+//eslint-disable-next-line
+import CardBoot from "react-bootstrap/Card";
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
-};
+  return (
+    <div>
+      <CardBoot style={{ width: "19rem" }}>
+        <CardBoot.Img
+          variant="top"
+          src={"http://openweathermap.org/img/wn/" + props.img + "@2x.png"}
+        />
+        <CardBoot.Body>
+          <CardBoot.Title>Card Title</CardBoot.Title>
+          <CardBoot.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </CardBoot.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </CardBoot.Body>
+      </CardBoot>
+    </div>
+  );
+}
